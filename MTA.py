b395d2272a67a43d25b88b64c8c8fa0932946f46
ben mccarthy <bmccarthy90@gmail.com>
2024-09-08 12:36:47 -0400

    push for merge

--- MTA.py
+++ MTA.py
@@ -1,6 +1,6 @@
 from google.transit import gtfs_realtime_pb2
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 feed = gtfs_realtime_pb2.FeedMessage()
 response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g')
@@ -14,6 +14,9 @@ def convert_timestamp(timestamp):
 def trip_id_convert(trip_id):
     return f'{datetime.fromtimestamp(int(trip_id[:-8]))} {trip_id[7:-1]}'
 
+def convert_seconds(seconds):
+    return timedelta(seconds = seconds)
+
 # def get_train_arrival_time(station):
 #     for stop in train["next_stops_array"]:
 #         if stop["stop_id"][:-1] == station:
@@ -26,22 +29,23 @@ def next_train_arrival(station, direction):
     arrival = []
     next_train_dict = {
         "train" : {},
-        "station_arrival_time" : ""
+        "station_arrival_time" : 0
     }
     for train in get_all_trains():
         if train["direction"] == direction:
-            # next_train = {}
-            min_time = 100
+            next_train = {}
+            min_time = ct 
             for stop in train["next_stops_array"]:
                 if stop["stop_id"][:-1] == station:
-                    
+                    min_time = ct + stop["arrival_time"]
+                    print(ct)
                     # print(train["trip_id"])
                     # print(stop["arrival_time"])
                     # print(stop["stop_id"])
-                    my_station_arrival_time = convert_timestamp(stop["arrival_time"]) - ct
-                    next_train.append(train)
-                    arrival.append(my_station_arrival_time)
-    return f'A {next_train[0]["direction"]} bound {next_train[0]["route_id"]} train will arrive at {station} in {arrival[0]}'
+                    # my_station_arrival_time = convert_timestamp(stop["arrival_time"]) - ct
+                    # next_train.append(train)
+                    # arrival.append(my_station_arrival_time)
+    # return f'A {next_train[0]["direction"]} bound {next_train[0]["route_id"]} train will arrive at {station} in {arrival[0]}'
     
 
 def get_all_trains():
@@ -83,6 +87,11 @@ print(train_test)
 
 # test_time_1 = 1725814530
 # test_time_2 = 1725815130
+
+# print(convert_timestamp(test_time_1))
+# print(convert_timestamp(test_time_2))
+# time_test = time_difference(test_time_1, test_time_2)
+# print(time_test)
 
 # print(convert_timestamp(test_time_1))
 # print(convert_timestamp(test_time_2))
