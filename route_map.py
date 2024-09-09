ce079ae0f30eef36db76202db2e18c14068ab5aa
Dave J <29786887+daveswork@users.noreply.github.com>
2024-09-09 17:24:10 -0400

    Build route sequence for G train.

--- route_map.py
+++ route_map.py
@@ -1,12 +1,43 @@
-from google.transit import gtfs_realtime_pb2
-import requests
-from datetime import datetime, timedelta
-from mta_output_formatting import time_difference, convert_seconds
-import pprint
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g')
-feed.ParseFromString(response.content)
+import sqlite3
+from MTA import get_stop_sequence
+from csv_to_db import mta_subway_station_query
 
 
 
+stops_in_sequence = get_stop_sequence()
+
+
+
+for stop in stops_in_sequence:
+    print(stop, mta_subway_station_query(stop)["stop_name"])
+
+
+def build_g_stop_database(stops_list):
+    connection = sqlite3.connect('mtainfo.db')
+    cursor = connection.cursor()
+    create_table = """
+    CREATE TABLE IF NOT EXISTS groute(
+    id INTEGER PRIMARY KEY,
+    stop_id TEXT,
+    stop_name TEXT
+    )
+    """
+
+
+    cursor.execute(create_table)
+    add_stop = """
+    INSERT INTO groute(
+    stop_id,
+    stop_name
+    )
+    VALUES (?, ?)
+    """
+
+    for stop in stops_list:
+        cursor.execute(add_stop, [stop[:-1], mta_subway_station_query(stop)["stop_name"]])
+
+    connection.commit()
+    connection.close()
+
+
+# build_g_stop_database(stops_in_sequence)
